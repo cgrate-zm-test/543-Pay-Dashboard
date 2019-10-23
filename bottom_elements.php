a0ac17ae606a597446de2aa636e457c5780fb31f
cgrate-peterd <peter.daka@cgrate.co.zm>
2019-10-23 21:49:32 +0200

    Completed all the dashboard cards. V2 Deployed.

--- bottom_elements.php
+++ bottom_elements.php
@@ -31,11 +31,11 @@
   <script src="assets/js/core/jquery.min.js"></script>
   <script src="assets/js/core/popper.min.js"></script>
   <script src="assets/js/core/bootstrap.min.js"></script>
-  <script src="assets/js/plugins/perfect-scrollbar.jquery.min.js"></script>
+  <script src="assets/js/core/perfect-scrollbar.jquery.min.js"></script>
   <!-- Chart JS -->
-  <script src="assets/js/plugins/chartjs.min.js"></script>
+  <script src="assets/js/core/chartjs.min.js"></script>
   <!--  Notifications Plugin    -->
-  <script src="assets/js/plugins/bootstrap-notify.js"></script>
+  <script src="assets/js/core/bootstrap-notify.js"></script>
   <!-- Control Center for Now Ui Dashboard: parallax effects, scripts for the example pages etc -->
   <script src="assets/js/paper-dashboard.min.js?v=2.0.0" type="text/javascript"></script>
   <!-- Labels for the charts -->
